54f92849885052863f762363ed7aaa5796583a99
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-25 14:38:12 +0000

    Added middleware to parse JSON request bodies

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Real Estate Land Management System!');
 });
